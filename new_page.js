889429bb376fa4a46033b1b7f1a17d3c46becd56
BryanIsRealomgomg <132394058+BryanIsRealomgomg@users.noreply.github.com>
2023-12-05 22:11:52 +0800

    Add files via upload

---
+++ new_page.js
@@ -0,0 +1,4 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Trigger fade-in effect for the text after the page is fully loaded
+    document.querySelector('.fade-in-text').classList.add('active');
+});
